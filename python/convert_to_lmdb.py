21264a8ad7f1e6dd6fd9f9ebf88b755376eb4500
rcarney <rmdcarney@gmail.com>
2015-07-28 17:26:00 -0700

    Added normalisation, also deploy is a copy of the train + test data

--- python/convert_to_lmdb.py
+++ python/convert_to_lmdb.py
@@ -19,9 +19,20 @@ LMDB_FILE_DEPLOY = '../data/wine_deploy_lmdb'
 #Pythony way to check if files exist
 #Since python <3 exceptions
 
-shutil.rmtree(LMDB_FILE_TRAIN)
-shutil.rmtree(LMDB_FILE_TEST)
-shutil.rmtree(LMDB_FILE_DEPLOY)
+try:
+    shutil.rmtree(LMDB_FILE_TRAIN)
+except:
+    pass
+
+try: 
+		shutil.rmtree(LMDB_FILE_TEST)
+except:
+    pass
+
+try:
+    shutil.rmtree(LMDB_FILE_DEPLOY)
+except:
+    pass
 
 #Record CSV
 with open(CSV_FILE) as csvfile:
@@ -41,20 +52,27 @@ with open(CSV_FILE) as csvfile:
     k = 0
     for row in csv_reader:
         
-        import_labels[i] = row[0]
+        import_labels[i] = int(row[0])-1
         for j in range(13):
-            import_data[i][j] = row[j+1]
+            import_data[i][j] = float(row[j+1])
         j=0
         i = i + 1
         
+#Shuffle data!
 state = np.random.get_state()
 np.random.shuffle(import_data)
 np.random.set_state(state)
 np.random.shuffle(import_labels)
 
+#Normalise data!
+import_data = import_data / import_data.max(axis=0)
+
+print import_labels[0], import_labels[1], import_labels[2]
 		
-train_labels, test_labels, deploy_labels = np.array_split(import_labels, [100, 150])
-train_data, test_data, deploy_data = np.array_split(import_data, [100, 150])
+train_labels, test_labels = np.array_split(import_labels, [150])
+train_data, test_data  = np.array_split(import_data, [150])
+deploy_data = import_data
+deploy_labels = import_labels
 
 map_size = 2**31 #Setting to the max seems to be the only way this doens't throw errors. 
 
@@ -62,8 +80,6 @@ train_blob = train_data.reshape(train_data.shape[0], train_data.shape[1], 1, 1).
 test_blob = test_data.reshape(test_data.shape[0], test_data.shape[1], 1, 1).astype(np.float)
 deploy_blob = deploy_data.reshape(deploy_data.shape[0], deploy_data.shape[1], 1, 1).astype(np.float)
 
-#TODO: remove
-print "train blob", train_blob[0], "\n", train_blob[1]
 k = 0
 train_env = lmdb.open(LMDB_FILE_TRAIN, map_size=map_size)
 with train_env.begin(write=True) as train_transaction:
